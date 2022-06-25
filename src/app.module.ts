89188bb90197a2eec62bfb9a4ef08e6d182046ce
IlhamShiddiq <shdqillham123@gmail.com>
2022-06-25 23:15:24 +0700

    feat/nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BooksModule, UsersModule, MongooseModule.forRoot('mongodb://admin:password@ilhamsh.id:27017', {
-    dbName: 'nest-book'
-  })],
+  imports: [
+    BooksModule, 
+    UsersModule, 
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION, {
+      dbName: process.env.MONGO_DBNAME
+    }), 
+  ],
   controllers: [],
   providers: [],
 })
