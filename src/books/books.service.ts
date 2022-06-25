2e438e03c26756e4424d351a844bc8e49befeb2c
IlhamShiddiq <shdqillham123@gmail.com>
2022-06-25 23:36:56 +0700

    feat/book request validator

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Book, BookDocument } from 'src/schemas/book.schema';
+import { BookValidator } from './dto/book-validator.dto';
 
 @Injectable()
 export class BooksService {
@@ -15,12 +16,12 @@ export class BooksService {
     return await this.BookModel.findById(id).exec()
   }
 
-  createBook(payload: any) {
+  createBook(payload: BookValidator) {
     const newBook = new this.BookModel(payload)
     return newBook.save()
   }
 
-  async editBookById(id: string, payload: any) {
+  async editBookById(id: string, payload: BookValidator) {
     return await this.BookModel.findByIdAndUpdate(id, payload).exec()
   }
 
